f30a971b3ca82ceb6a2687e74dc28db6b36d91a5
eferlius <ferlinghetti.enrico@gmail.com>
2024-01-12 14:29:57 +0100

    pd ext\fun copy paste\apply map sign digits

--- basic_pandas_ext/functions_copy_paste.py
+++ basic_pandas_ext/functions_copy_paste.py
@@ -11,3 +11,7 @@ def function_copy_paste_concat_df_for_loop_iteration(i, df_to_add, df_concat):
     else:
         df_concat = pd.concat([df_concat, df_to_add], axis=0)
     return df_concat
+
+def function_copy_paste_apply_map_for_significant_digits(df, decimals):
+    format_string = '{'+':.{}f'.format(decimals)+'}'
+    df.applymap(format_string.format)
