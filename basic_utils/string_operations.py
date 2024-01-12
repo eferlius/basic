5a23f4f31f56c85044ee91bc3f113bfc0ab9d43a
eferlius <ferlinghetti.enrico@gmail.com>
2024-01-12 18:31:19 +0100

    find char around sub with n and skip

--- basic_utils/string_operations.py
+++ basic_utils/string_operations.py
@@ -1,9 +1,35 @@
+def find_characters_around_substring(s, substring, n, skip = 0):
+    '''
+    returns the "n" characters in "s" after or before "substring", skipping "skip" characters
 
-def find_characters_around_substring(s, substring, n, direction='after'):
-    index = s.find(substring)
+    Parameters
+    ----------
+    s : string
+        complete string
+    substring : string
+        substring to be used for searching
+    n : int (positive or negative)
+        how many characters after or before (respectively) the occurrence of substring
+        if n positive, after
+        if n negative, before
+    skip: int (positive) or -1
+        how many character are skipped after or before (respectively) writing the substring
+
+    Returns
+    -------
+    substring
+    '''
+    skip = abs(skip)
+    if n>0:
+        direction = 'after'
+    if n<0:
+        direction = 'before'
+        n=-n
+        skip = - skip
+    index = s.find(substring) + skip
 
     if index == -1:
-        return "Substring not found in the given string."
+        return None
 
     if direction == 'before':
         start_index = max(0, index - n)
@@ -12,6 +38,6 @@ def find_characters_around_substring(s, substring, n, direction='after'):
         end_index = min(len(s), index + len(substring) + n)
         result = s[index + len(substring):end_index]
     else:
-        return "Invalid direction. Choose 'before' or 'after'."
+        return None
 
     return result
